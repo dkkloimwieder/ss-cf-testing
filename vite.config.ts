44f8b0616facfd0c32551480bd39fa62486a05f8
d kloimwieder <dkkloimwieder@gmail.com>
2022-04-10 08:49:05 -0400

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import solid from "solid-start";
+
+export default defineConfig({
+  plugins: [
+    {
+      ...(await import("@mdx-js/rollup")).default({
+        jsx: true,
+        jsxImportSource: "solid-js",
+        providerImportSource: "solid-mdx",
+      }),
+      enforce: "pre",
+    },
+    solid({
+      extensions: [".mdx", ".md"],
+      adapter: "solid-start-cloudflare-workers",
+      prerenderRoutes: [],
+      ssr: true,
+    }),
+  ],
+});
